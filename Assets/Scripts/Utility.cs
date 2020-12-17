f05da23724ff42abb8f124c91c75e220d3c34962
Evan Cheng <dr.frolicks@gmail.com>
2020-12-17 02:45:18 -0800

    added universalTimeMS

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+
 public static class Utility
 {
     public static void Invoke(this MonoBehaviour mb, Action f, float delay)
@@ -15,6 +16,38 @@ public static class Utility
         yield return new WaitForSeconds(delay);
         f();
     }
+    
+    public static void ResetTransformation(this Transform trans)
+    {
+        trans.position = Vector3.zero;
+        trans.localRotation = Quaternion.identity;
+        trans.localScale = new Vector3(1, 1, 1);
+    }
+
+    /// <summary>
+    /// Returns the time in milliseconds since the start of the hour in UTC timezone.
+    /// todo optimize, might be laggy if called every frame 
+    /// </summary>
+    /// <param name="???"></param>
+    /// <param name="???"></param>
+    /// <returns></returns>
+    public static int universalTimeMS() 
+    {
+        var uTime = System.DateTime.UtcNow;
+        return uTime.Second * 1000 + uTime.Millisecond; 
+    }
+
+    /// <summary>
+    /// Returns the lerp t value based on universalTimeMS with a given intervalTime. 
+    /// </summary>
+    /// <param name="intervalTime"> in seconds, only 3 decimal places/param>
+    /// <returns></returns>
+    // public static float getSinLerpT(float intervalTimeSeconds)
+    // {
+    //     int intervalTimeMS = (int)(intervalTimeSeconds * 1000); 
+    //     
+    //     float x = (2 * Mathf.PI / intervalTimeMS) % intervalTimeSeconds - (Mathf.PI / 2) 
+    // }
 }
 
 [System.Serializable] public class GameObjectEvent : UnityEvent<GameObject> {}
