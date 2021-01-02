9b4eb19846d9a502ec5c91245afd282919496b3d
CLIECKER\evanw <woovan57@gmail.com>
2021-01-02 02:47:47 -0800

    Revert "no message"
    This reverts commit 20aa8c4fad4823bad3e9759096bb94031322b738.

--- Assets/Mirror.cs
+++ Assets/Mirror.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
-using Random = UnityEngine.Random;
+using Random = System.Random;
 using Vector3 = UnityEngine.Vector3;
 
 public class Mirror : MonoBehaviour
@@ -12,28 +12,15 @@ public class Mirror : MonoBehaviour
     private Vector3 normalVector;
     private Vector3 newestVector;
     public float power;
-    public Renderer rend;
-    
-    public void Start()
-    {
-        rend = GetComponent<Renderer>();
-    }
     public void OnTriggerEnter(Collider other)
     {
         hitPos = other.ClosestPointOnBounds(transform.position);
         normalVector = hitPos - transform.position;
         normalVector.y = 0f;
+        Debug.DrawRay(hitPos,normalVector*10,Color.blue);
+        print(other.transform.forward);
         newestVector = other.transform.forward - 2*(Vector3.Dot(other.transform.forward,normalVector))*normalVector/Mathf.Pow(Vector3.Magnitude(normalVector),2);
         other.transform.forward = newestVector;
-        // if (other.gameObject.CompareTag("Damage"))
-        // {
-        //     
-        // }
-        if (newestVector == null)
-        {
-            return;
-        }
         other.transform.GetComponent<Rigidbody>().AddForce(newestVector*power,ForceMode.Impulse);
-        rend.material.SetColor("_BaseColor",Random.ColorHSV());
     }
 }
