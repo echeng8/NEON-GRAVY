e236665babdbfa89ea3e78974dc20083965f8ec5
Andrew Stefani <philosophysphere@gmail.com>
2021-01-13 21:55:26 -0800

    Added Mute Button, made chat work visually.

---
+++ Assets/Scripts/Player/MuteScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteScript : MonoBehaviour
+
+{
+    public void MuteAllSound()
+    {
+        AudioListener.volume = 0;
+    }
+
+    public void UnMuteAllSound()
+    {
+        AudioListener.volume = 1;
+    }
+}
