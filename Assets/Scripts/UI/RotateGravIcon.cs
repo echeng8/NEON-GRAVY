10135457247c5e4acf1fcb9890b002d810d4785a
Andrew Stefani <philosophysphere@gmail.com>
2021-01-14 19:21:02 -0800

    implemented mute music only button, and flipping of grav icon between states

---
+++ Assets/Scripts/UI/RotateGravIcon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGravIcon : MonoBehaviour
+{
+    Vector3 rotationEuler;
+    public void hudGravSwitch(bool gravOn)
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.Rotate(new Vector3(0, 0, 180));
+    }
+    void AddPlayerListeners(GameObject p)
+    {
+        p.GetComponent<PlayerGravity>().OnGravityChange.AddListener(hudGravSwitch);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerIdentity.CallOnLocalPlayerSet(AddPlayerListeners);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
