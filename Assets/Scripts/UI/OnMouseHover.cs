33f435343e1541e9e355dfd9f22a2f556eec0cee
Andrew Stefani <philosophysphere@gmail.com>
2021-01-19 21:28:46 -0800

    WIP sound design, more work on platforms

---
+++ Assets/Scripts/UI/OnMouseHover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class OnMouseHover : MonoBehaviour, IPointerEnterHandler 
+{
+    
+    public UnityEvent OnMouseOver = new UnityEvent();
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnMouseOver.Invoke();
+    }
+    
+
+}
