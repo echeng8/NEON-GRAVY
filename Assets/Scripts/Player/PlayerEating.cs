6190dfc576d06eb4cbbaf7d041d05b0463059677
Evan Cheng <dr.frolicks@gmail.com>
2021-03-25 18:41:55 -0700

    player eating by color

--- Assets/Scripts/Player/PlayerEating.cs
+++ Assets/Scripts/Player/PlayerEating.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Realtime;
 using Photon.Pun; 
+
 /// <summary>
-/// Agario-style functionality
-/// When player touches another with less gravy then the one with bigger gravy kills the other. 
+/// Kills other players when they are the color you beat. 
 /// </summary>
 public class PlayerEating : MonoBehaviourPun
 {
-    /// <summary>
-    /// Dot product of forward vectors on collision must be greater than this amount to trigger a kill. 
-    /// </summary>
-    public float LethalDotProduct; 
 
+    PlayerColorChange playerColorChange; 
     PlayerDeath playerDeath;
     PlayerIdentity playerIdentity;
 
@@ -21,6 +18,8 @@ public class PlayerEating : MonoBehaviourPun
 
     private void Awake()
     {
+
+        playerColorChange = GetComponent<PlayerColorChange>(); 
         playerDeath = GetComponentInParent<PlayerDeath>();
         playerIdentity = GetComponentInParent<PlayerIdentity>(); 
     }
@@ -41,21 +40,35 @@ public class PlayerEating : MonoBehaviourPun
                 return;
             }
 
-            //This player is eaten. 
-            if(Utility.IsFacingSameDirection(MyBody.forward, other.transform.forward, LethalDotProduct) &&
-               !Utility.IsBehind(MyBody.forward, MyBody.position, other.transform.position))
+            PlatformState myState = playerColorChange.PlatState;
+            PlatformState theirState = other.GetComponentInParent<PlayerColorChange>().PlatState; 
+
+            if(myState == theirState) //bounce off
             {
+                GetComponent<PlayerMoveSync>().UpdateMovementRPC(GetComponent<PlayerMovement>().Velocity.normalized * -GetComponent<PlayerMovement>().maxTopSpeed, transform.position);
+            } else if (IsEatenBy(myState, theirState)) {
+                //This player is eaten. 
                 Camera.main.GetComponent<FreezeFrame>().FreezeCamera();
                 playerDeath.KillPlayer(otherPV.OwnerActorNr); //kill self
-                return;  
-            }
-
-            //We bounce off if they have more gravies
-            if(other.GetComponentInParent<PlayerIdentity>().Gravies > playerIdentity.Gravies)
-            {
-                GetComponent<PlayerMoveSync>().UpdateMovementRPC(GetComponent<PlayerMovement>().Velocity.normalized * -GetComponent<PlayerMovement>().maxTopSpeed, transform.position); 
             }
         }
+    }
+
+
+    /// <summary>
+    /// returns true if selfstate is eaten by theirstate
+    /// </summary>
+    /// <param name="selfState"></param>
+    /// <param name="theirState"></param>
+    /// <returns></returns>
+    public bool IsEatenBy(PlatformState selfState, PlatformState theirState)
+    {
+        if (selfState == PlatformState.FIRE && theirState == PlatformState.WATER ||
+            selfState == PlatformState.GRASS && theirState == PlatformState.FIRE || 
+            selfState == PlatformState.WATER && theirState == PlatformState.GRASS)
+            return true;
+
+        return false; 
     }
     #endregion
 }
