c30a660823774faf79e7e27248a8fc856668b938
CLIECKER\evanw <woovan57@gmail.com>
2021-03-29 03:53:18 -0700

    Launcher Buttons

---
+++ Assets/WebsiteLinks.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebsiteLinks : MonoBehaviour
+{
+    public Button officialWebsiteButton;
+    public Button redditButton;
+    public Button twitterButton;
+    public Button appleButton;
+    public Button googleButton;
+    public Button discordButton;
+
+    public string officialWebsiteURL;
+    public string redditURL;
+    public string twitterURL;
+    public string appleURL;
+    public string googleURL;
+    public string discordURL;
+
+    
+    void Start()
+    {
+        officialWebsiteButton.onClick.AddListener(officialLink);
+        redditButton.onClick.AddListener(redditLink);
+        twitterButton.onClick.AddListener(twitterLink);
+        appleButton.onClick.AddListener(appleLink);
+        googleButton.onClick.AddListener(googleLink);
+        discordButton.onClick.AddListener(discordLink);
+    }
+
+    void officialLink()
+    {
+        Application.OpenURL(officialWebsiteURL);
+    }
+    void redditLink()
+    {
+        Application.OpenURL(redditURL);
+    }
+    void twitterLink()
+    {
+        Application.OpenURL(twitterURL);
+    }
+    void appleLink()
+    {
+        Application.OpenURL(appleURL);
+    }
+
+    void googleLink()
+    {
+        Application.OpenURL(googleURL);
+    }
+
+    void discordLink()
+    {
+        Application.OpenURL(discordURL);
+    }
+}
