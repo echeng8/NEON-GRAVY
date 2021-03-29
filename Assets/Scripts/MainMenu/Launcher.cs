518a5a373197ee8f711c7e7afd2d8d878473c8ae
Evan Cheng <dr.frolicks@gmail.com>
2021-03-29 02:36:51 -0700

    join codes fully implemented i think

--- Assets/Scripts/MainMenu/Launcher.cs
+++ Assets/Scripts/MainMenu/Launcher.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using Random = UnityEngine.Random;
-using UnityEngine.Events; 
-
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro; 
 public class Launcher : MonoBehaviourPunCallbacks
 {
     public UnityEvent OnConnectedSuccess = new UnityEvent();
+    public TMP_InputField roomCodeInput;
 
-
-    private const String randomCharacterString = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
+    const String randomCharacterString = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
 
     private void Awake()
     {
@@ -58,6 +59,11 @@ public class Launcher : MonoBehaviourPunCallbacks
         print($"My current room name is {PhotonNetwork.CurrentRoom.Name}");
     }
 
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        print("OnJoinRoomFailed"); 
+        roomCodeInput.text = "";  
+    }
 
     public override void OnCreatedRoom()
     {
@@ -101,8 +107,15 @@ public class Launcher : MonoBehaviourPunCallbacks
         // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
         if (PhotonNetwork.IsConnectedAndReady)
         {
-            // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
-            PhotonNetwork.JoinRandomRoom();
+            print(roomCodeInput.text);
+            print(roomCodeInput.text.Length);
+            if (roomCodeInput.text != "") 
+            {
+                PhotonNetwork.JoinRoom(roomCodeInput.text.ToUpper()); 
+            } else
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
         }
     }
 
@@ -111,8 +124,10 @@ public class Launcher : MonoBehaviourPunCallbacks
         PhotonNetwork.NickName = name; 
         PlayerPrefs.SetString("Name", name);
     }
-    #endregion
 
+
+    #endregion
+    #region Private Methods
 
     /// <summary>
     /// returns a random string of 4 numbers
@@ -128,4 +143,5 @@ public class Launcher : MonoBehaviourPunCallbacks
         return code; 
         
     }
+    #endregion
 }
